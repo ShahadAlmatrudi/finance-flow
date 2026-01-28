f59d6cba4a414b2d89092d64e52a68bbc5b7839e
ShahadAlmatrudi <s202264980@kfupm.edu.sa>
2026-01-28 18:20:48 +0300

    Initial project setup

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+// Middleware to read JSON
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Money Planner API is running");
+});
+
+// Start server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
